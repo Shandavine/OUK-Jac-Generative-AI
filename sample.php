a8dc149c3d7a19e15af377eda5adbc8fc42917f4
Shandavine <shandavinechemutai1@gmail.com>
2025-11-05 22:10:52 +0300

    Added Assignment2.jac file

---
+++ sample.php
@@ -0,0 +1,15 @@
+<?php
+// Sample PHP file for Jac assignment
+
+function greetUser($name) {
+    echo "Hello, " . $name . "!";
+}
+
+function addNumbers($a, $b) {
+    return $a + $b;
+}
+
+function getDateToday() {
+    return date("Y-m-d");
+}
+?>
